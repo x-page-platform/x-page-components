ecde1dc30ab6bc84540e597f0cb4430e24485792
jinfeng <jinfeng@qima-inc.com>
2017-04-12 23:05:19 +0800

    feat: add test

--- src/base/dataSet.js
+++ src/base/dataSet.js
@@ -1,44 +1,79 @@
 import EventEmitter from 'events';
+import defaults from 'lodash/defaults';
+import assign from 'lodash/assign';
+import axios from 'axios';
 
-export class DataSet extends EventEmitter {
+let cache = {};
+
+export class Record extends EventEmitter {
+  constructor(data) {
+    super();
+    this.data = data;
+  }
+}
+
+export default class DataSet extends EventEmitter {
   defaults() {
     return {
+      // 自动分页
+      autoPage: false,
+      // 自动创建一条记录
       autoCreate: true,
+      // 自动查询
       autoQuery: false,
+      // 分页大小
       pageSize: 10,
+      // 查询的url
       queryUrl: '',
+      // 是否可选，grid组件使用
       selectable: false,
+      // post url
       postUrl: '',
+      // 查询的dataset
       queryDataSet: null,
+      // cascade dataset
       bindDataSet: null,
+      // cascade field
       bindDataSetField: ''
     };
   }
 
   constructor(config) {
     super();
-    this.data = config.data || {};
+    this.config = defaults({}, config, this.defaults());
+    // 当前记录index
+    this.index = 0;
+    // 当前分页
+    this.page = 1;
+    // 记录
+    this.records = [];
+    // 绑定的组件
+    this.bindCmps = [];
   }
 
   /**
    * add record
    */
-  add() {
-
+  add(record) {
+    if (record instanceof Record) {
+      this.records.push(record);
+    } else {
+      this.records.push(new Record(record));
+    }
   }
 
   /**
    * @param record
    */
-  remove() {
-
+  remove(record) {
+    this.records.splice(this.findIndex(record), 1);
   }
 
   /**
    * remove all record
    */
   removeAll() {
-
+    this.records = [];
   }
 
   /**
@@ -46,7 +81,7 @@ export class DataSet extends EventEmitter {
    * @return record
    */
   create() {
-
+    this.add(new Record(this.config.data));
   }
 
   /**
@@ -54,15 +89,16 @@ export class DataSet extends EventEmitter {
    * @return record
    */
   getCurrentRecord() {
-
+    return this.records[this.index];
   }
 
   /**
    * locate index to record
    * @param index
    */
-  locate() {
-
+  locate(index) {
+    this.emit('indexChange');
+    this.index = index;
   }
 
   /**
@@ -70,8 +106,14 @@ export class DataSet extends EventEmitter {
    * @param value
    * @return record
    */
-  find() {
+  findRecordByName(name, value) {
+    return this.records.find(record => {
+      return value === record.get(name);
+    });
+  }
 
+  findIndex(record) {
+    return this.records.indexOf(record);
   }
 
   /**
@@ -81,18 +123,41 @@ export class DataSet extends EventEmitter {
 
   }
 
+  prepareQueryParams() {
+    let params = {};
+    const { bindDataSet, bindDataSetField, queryDataSet, autoPage, pageSize } = this.config.bindDataSetField;
+    if (queryDataSet) {
+      assign(params, queryDataSet.getCurrentJson());
+    }
+    if (bindDataSet && bindDataSetField) {
+      assign(params, { [bindDataSetField]: bindDataSet.getCurrentJson[bindDataSetField] });
+    }
+    if (autoPage) {
+      assign(params, { pageSize, page: this.page });
+    }
+    return params;
+  }
+
   /**
    * query server data
    */
   query() {
-
+    this.emit('beforeAjax');
+    let params = this.prepareQueryParams();
+    axios.get(this.config.queryUrl, {
+      params
+    }).then(response => {
+      console.log(response);
+    }).catch((e) => {
+      console.log(e);
+    });
   }
 
   /**
    * post data to server
    */
   post() {
-
+    this.emit('beforeAjax');
   }
 
   /**
@@ -131,4 +196,16 @@ export class DataSet extends EventEmitter {
   getSelected() {
 
   }
+
+  /**
+   * destory dataset
+   */
+  destory() {
+
+  }
+}
+
+let _uuid = 0;
+function uuid() {
+  return _uuid++;
 }
--- test/dataset.js
+++ test/dataset.js
@@ -1,1 +1,17 @@
-let dataset1 = new DataSet();
+import DataSet from '../src/base/dataSet';
+
+let queryDataSet = new DataSet({
+  id: 'form',
+  data: {
+    title: '',
+    name: ''
+  }
+});
+
+let dataset1 = new DataSet({
+  queryDataSet: 'form',
+  queryUrl: ''
+});
+
+
+dataset1.query();
